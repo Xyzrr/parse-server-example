5435b3d74491ccf6a7b5ccfcc7b32e9b746cec64
John Qian <johnlongqian@gmail.com>
2016-07-14 13:45:06 -0400

    Installed Dashboard

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+require('parse-dashboard/Parse-Dashboard/index.js');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
